ea23c1246ff325f7262e6621dac719004858bf4d
Sparsh Jain <sjain1@ec.iitr.ac.in>
2020-05-08 17:32:59 +0530

    Thunk and Logger

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,22 +1,8 @@
 import React, { Component, useCallback } from "react";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  Breadcrumb,
-  BreadcrumbItem,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Button,
-  Row,
-  Col,
-  Label
-} from "reactstrap";
-import { Control, LocalForm, Errors } from "react-redux-form";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Button, Row, Col, Label} from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent';
 
 // import CommentForm from "./CommentFormComponent";
 
@@ -61,33 +47,59 @@ return (
   
 }
 
-const DishDetail = props => {
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/menu">Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.name}</h3>
-          <hr />
+const DishDetail = (props) => {
+  
+  if(props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+            <Loading />
+          </div>
+      </div>
+    );
+  } 
+  else if(props.errMess) {
+    return (
+      <div className="container">
+          <div className="row">
+            <h4>{props.errMess}</h4>
+          </div>
+      </div>
+    );
+  }
+  else if(props.dish!=null){
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish {...props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComments comments={props.comments} 
+              addComment={props.addComment}
+              dishId={props.dish.id}  />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <RenderDish {...props.dish} />
-        </div>
-        <div className="col-12 col-md-5 m-1">
-          <RenderComments comments={props.comments} 
-            addComment={props.addComment}
-            dishId={props.dish.id}  />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
+  else{
+    return(
+      <div></div>
+    );
+  }
 };
 
 // CommentForm Component and it's helper functions
