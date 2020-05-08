11fb87fc2e58e7f4896b8b822513fdf955ae1a93
Sparsh Jain <sjain1@ec.iitr.ac.in>
2020-05-08 10:30:43 +0530

    Combining Reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,19 @@
-import { createStore } from 'redux';
-import { Reducer, initialState} from './reducer'
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+import { Comments } from './comments';
+import { PROMOTIONS } from './promotions';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
